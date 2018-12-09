35d4dfbdd7f1881d0b37d49b68d3b0828ce12549
Yeison Garcia Santos <yeison.santos@outlook.com>
2018-12-09 00:52:14 -0500

    Option for Recursive Method

--- SucesionFibonacci/Metodos.cs
+++ SucesionFibonacci/Metodos.cs
@@ -8,22 +8,49 @@ namespace SucesionFibonacci
 {
     public class Metodos
     {
-        public void Imprimir(int Limite)
+        int Num1 = 0;
+        int Num2 = 1;
+        int aux = 0;
+
+        public void StatementFor(int Limite)
         {
-            int Num1 = 0;
-            int Num2 = 1;
-            int din = 0;
+
 
             Console.WriteLine(Num1 + "\n" + Num2);
 
-            for (int i = 0; din < Limite; i++)
+            for (int i = 0; aux < Limite; i++)
             {
-                din = Num1 + Num2;
+                aux = Num1 + Num2;
                 Num1 = Num2;
-                Num2 = din;
-                Console.WriteLine(din);
+                Num2 = aux;
+                Console.WriteLine(aux);
             }
-       
+
         }
+
+        public void RecursiveM(int Limite)
+        {
+            aux = Num1;
+            Num1 = Num2; 
+            Num2 = aux + Num1; 
+            Console.WriteLine(aux); 
+
+            while (aux<=Limite)
+            {
+                RecursiveM(Limite);
+            }
+
+        }
+
+        public int Limite()
+        {
+            Console.WriteLine("Now write the limit of succession:");
+            int Limite;
+            Limite = int.Parse(Console.ReadLine());
+
+            return Limite;
+        }
+
+
     }
 }
--- SucesionFibonacci/Program.cs
+++ SucesionFibonacci/Program.cs
@@ -10,11 +10,33 @@ namespace SucesionFibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Escribe el limite de la Sucesion:");
+            Metodos Stat = new Metodos();
             int Limite;
-            Limite = int.Parse(Console.ReadLine());
-            Metodos ins = new Metodos();
-            ins.Imprimir(Limite);
+
+            Console.WriteLine("Select an option: " + "\n" + "1. Whith Statement for." + "\n" + "2. Whith Recursive Method.");
+            
+            int opcion;
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+
+                    Limite = Stat.Limite();
+                    Stat.StatementFor(Limite);
+
+                    break;
+
+                case 2:
+
+                    Limite = Stat.Limite();
+                    Stat.RecursiveM(Limite);
+
+                    break;
+
+                default:
+                    break;
+            }
 
             Console.ReadKey();
 
